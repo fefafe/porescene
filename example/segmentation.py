ad8a1c01207c6fe34a0737d0352c46067b5fdcd0
Felix Faber <felix.faber@ovgu.de>
2026-07-12 14:09:15 +0200

    Add example script for void segmentation

---
+++ example/segmentation.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from porescene import image
+from porescene.color.palette import Colormap, Palette
+from porescene.scene import Scene
+
+
+# ==============================================================
+# Import Parameters
+
+# data directory
+pth_data = Path.cwd() / "data"
+
+# temporary directory for rendered images
+pth_tmp = Path.cwd() / "tmp"
+
+# [m] domain dimensions
+dims = (100e-06, 100e-06, 100e-06)
+
+
+# ==============================================================
+# Scene configuration and rendering
+
+# create a new scene from PoreScene JSON configuration
+sc = Scene.from_json(dims, pth_data / "porescene.json")
+
+# add axes around the scene
+sc.create_axes()
+
+# add a solid object to the scene
+sc.create_clusters(pth_data / "segmentation-void.obj")
+sc.apply_colors("Clusters", Palette.load(Colormap.BATLOW).random(4000))
+
+# render the scene
+pth_img = sc.render(pth_tmp, "cluster-random")
+
+# trim whitespace from the renderen PNG, add a padding of 50 px
+image.img_trim(pth_img, 50)
