1e8d0857953c96780fbfa658216644aec1cdf7c5
Felix Faber <felix.faber@ovgu.de>
2026-07-17 16:25:30 +0200

    Refactor module palette

---
+++ src/porescene/color/palette/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# Copyright (C) 2026 Felix Faber /
+# Otto von Guericke University Magdeburg, Thermal Process Engineering
+
+from ._palette import Colormap, Palette
+
+__all__ = ["Colormap", "Palette"]
