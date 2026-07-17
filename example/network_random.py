1503f51a13287585cc513680f3d095122e9a2388
Felix Faber <felix.faber@ovgu.de>
2026-07-18 01:57:31 +0200

    Add example script for randomly colored network structure

---
+++ example/network_random.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+
+from porescene import worker, image
+from porescene.color.palette._palette import Colormap, Palette
+from porescene.model import PoreNetwork
+from porescene.scene import Scene
+
+# =============================================================================
+# Import Parameters
+
+# data directory
+pth_data = Path.cwd() / "data"
+
+# temporary directory for rendered images
+pth_tmp = Path.cwd() / "tmp"
+
+
+# =============================================================================
+# Scene configuration
+
+# load variable mapping for variable import from .mat file
+with open(pth_data / "map_vars.json") as f:
+    map_vars = json.load(f)
+
+# load pore network data from MAT file
+pn = PoreNetwork.from_mat(pth_data / "pnm.mat", map_vars["data_network"])
+
+# load PoreScene config from JSON file
+sc = Scene.from_json(pn.extent, pth_data / "porescene.json")
+
+# set the default color palette for the scene
+sc.config_scene.palette = Palette.load(Colormap.ROMAO)
+
+# add cylinders and spheres to the scene
+worker.build_structure(sc, pn)
+
+# add axes around the scene
+sc.create_axes()
+
+# render the scene
+pth_img = worker.make_random(pth_tmp, pn, sc)
+
+# add padding of 10 %
+image.img_pad(pth_img, 0.1)
