538be9e4b94b80db94bd5fefdcdfcd3bef0e6495
Felix Faber <felix.faber@ovgu.de>
2026-07-23 14:18:21 +0200

    Update network topology example script

--- example/network_topology.py
+++ example/network_topology.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 
 from porescene import worker
+from porescene.color.palette import fefa
 from porescene.model import PoreNetwork
 from porescene.scene import Scene
 
@@ -29,7 +30,7 @@ pn.pore_radius = np.ones(pn.pore_count) * 0.4e-6
 pn.throat_radius = np.ones(pn.throat_count()) * 0.2e-6
 
 # load PoreScene config from JSON file
-sc = Scene.from_json(pn.extent, pth_data / "porescene.json")
+sc = Scene(pn.extent)
 
 # add cylinders and spheres to the scene
 worker.build_structure(sc, pn)
@@ -37,5 +38,16 @@ worker.build_structure(sc, pn)
 # add axes around the scene
 sc.create_axes()
 
-# render the scene
-pth_img = worker.make_structure(pth_data, pn, sc)
+# color every pore orange and every throat dark green
+color_pores = [fefa.orange for _ in range(pn.pore_count)]
+color_throats = [fefa.darkgreen for _ in range(pn.throat_count())]
+
+# render the scene with uniform pore and throat colors
+worker.make_img(
+    pth_data,
+    sc,
+    color_spheres=color_pores,
+    color_cylinders=color_throats,
+    name_spheres="structure",
+    name_cylinders="structure",
+)
